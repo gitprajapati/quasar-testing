128cdb81f5382971eecb7cd1894e0f4961695fe3
gitprajapati <21f1003546@student.onlinedegree.iitm.ac.in>
2024-10-04 17:06:39 +0530

    scroll tested

--- src/pages/profilePage.vue
+++ src/pages/profilePage.vue
@@ -1,60 +1,55 @@
 <template>
-  <q-page class="q-pa-sm scroll">
-    <q-pull-to-refresh
-      @refresh="fetchOrderHistory"
-      scroll-target="#order-history-list"
-    >
-      <div id="order-history-list" class="scroll-y">
-        <div class="row q-col-gutter-sm">
-          <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
-            <q-card class="card-bg text-white no-shadow" bordered>
-              <q-card-section class="text-center bg-transparent">
-                <q-avatar size="100px" class="shadow-10">
-                  <img src="profile.svg" />
-                </q-avatar>
-                <div class="text-h6 q-mt-md">Abhishek Prasad</div>
-              </q-card-section>
-              <q-card-section>
-                <div>
-                  <strong>{{ $t("name") }}:</strong>
-                </div>
-                <div><strong>Sang Pravesh:</strong></div>
-                <div><strong>Daitva:</strong></div>
-                <div><strong>Branch:</strong></div>
-              </q-card-section>
-            </q-card>
-          </div>
-          <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
-            <q-card class="card-bg text-white no-shadow" bordered>
-              <q-card-section>
-                <div class="text-h6">Info</div>
-                <div>
-                  <strong>Phone:</strong>
-                </div>
-                <div><strong>Highest Education:</strong></div>
-                <div><strong>Sikchad:</strong></div>
-                <div><strong>Profession:</strong></div>
-                <div><strong>Years Of Exp.:</strong></div>
-                <div><strong>Jobs Completed:</strong></div>
-              </q-card-section>
-            </q-card>
-          </div>
-          <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
-            <q-card class="card-bg text-white no-shadow" bordered>
-              <q-card-section>
-                <div class="text-h6">Address</div>
-                <div>
-                  <strong>State:</strong>
-                </div>
-                <div><strong>District:</strong></div>
-                <div><strong>Pincode:</strong></div>
-                <div><strong>City:</strong></div>
-                <div><strong>Nagar:</strong></div>
-                <div><strong>Basti:</strong></div>
-                <div><strong>Gali:</strong></div>
-              </q-card-section>
-            </q-card>
-          </div>
+  <q-page class="q-pa-sm"
+    ><q-pull-to-refresh @refresh="fetchOrderHistory">
+      <div class="row q-col-gutter-sm">
+        <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+          <q-card class="card-bg text-white no-shadow" bordered>
+            <q-card-section class="text-center bg-transparent">
+              <q-avatar size="100px" class="shadow-10">
+                <img src="profile.svg" />
+              </q-avatar>
+              <div class="text-h6 q-mt-md">Abhishek Prasad</div>
+            </q-card-section>
+            <q-card-section>
+              <div>
+                <strong>{{ $t("name") }}:</strong>
+              </div>
+              <div><strong>Sang Pravesh:</strong></div>
+              <div><strong>Daitva:</strong></div>
+              <div><strong>Branch:</strong></div>
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+          <q-card class="card-bg text-white no-shadow" bordered>
+            <q-card-section>
+              <div class="text-h6">Info</div>
+              <div>
+                <strong>Phone:</strong>
+              </div>
+              <div><strong>Highest Education:</strong></div>
+              <div><strong>Sikchad:</strong></div>
+              <div><strong>Profession:</strong></div>
+              <div><strong>Years Of Exp.:</strong></div>
+              <div><strong>Jobs Completed:</strong></div>
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+          <q-card class="card-bg text-white no-shadow" bordered>
+            <q-card-section>
+              <div class="text-h6">Address</div>
+              <div>
+                <strong>State:</strong>
+              </div>
+              <div><strong>District:</strong></div>
+              <div><strong>Pincode:</strong></div>
+              <div><strong>City:</strong></div>
+              <div><strong>Nagar:</strong></div>
+              <div><strong>Basti:</strong></div>
+              <div><strong>Gali:</strong></div>
+            </q-card-section>
+          </q-card>
         </div>
       </div></q-pull-to-refresh
     >
@@ -76,7 +71,7 @@ export default defineComponent({
     fetchOrderHistory(done) {
       setTimeout(() => {
         done();
-      }, 2000);
+      }, 1000);
     },
   },
 });
